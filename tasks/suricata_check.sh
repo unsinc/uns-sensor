42617a171ffd759f4050fd9e65b7417a500b8104
unsinc <156654990+unsinc@users.noreply.github.com>
2024-03-21 15:40:30 -0500

    Update suricata_check.sh

--- tasks/suricata_check.sh
+++ tasks/suricata_check.sh
@@ -3,5 +3,5 @@ ps -ef | grep suricata | grep -v grep
 if [ $? -eq 0 ]; then
   echo "Process is running."
 else
-  sudo docker exec IDS /bin/bash -c "service suricata start"
+  sudo docker exec IDS /bin/bash -c "service suricata start" | logger -t "Suricata Restarting service after it has failed to run"
 fi
