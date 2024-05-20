d02747797f888f5cf8cd2f7c6e18961a3e915404
unsinc <156654990+unsinc@users.noreply.github.com>
2024-05-20 10:30:23 -0500

    Create patch_1.0.sh

---
+++ configuration/patch_1.0.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Stop and delete the old IDS Sensor container
+echo Stopping and deleting the old IDS contaner
+
+sudo docker stop IDS & pid0=$!
+
+wait $pid0
+
+sudo docker remove IDS & pid1=$!
+
+wait $pid1
+
+# remove the old suricata container image
+echo Removing the old IDS image
+
+sudo docker image rm suricata & pid2=$!
+
+wait $pid2
+
+# clear the old docker system chache
+
+echo Clearing the old docker cache
+
+sudo docker system prune -a -f & pid3=$!
+
+wait $pid3
+
+# dowload the updated docker compose file
+echo Downloading the new docker-compose file and dockerfile
+
+sudo curl https://raw.githubusercontent.com/unsinc/uns-sensor/lab/configuration/docker-compose.yml -o /sns/docker/docker-compose.yml & pid4=$!
+
+wait $pid4 
+
+sudo curl https://raw.githubusercontent.com/unsinc/uns-sensor/lab/configuration/dockerfile -o /sns/docker/dockerfile & pid5=$!
+
+wait $pid5
+
+# create the new IDS container
+echo Running docker compose
+
+sudo docker compose -f /sns/docker/docker-compose.yml up -d & pid6=$!
+
+wait $pid5
+
+sudo curl https://raw.githubusercontent.com/unsinc/uns-sensor/lab/tasks/suricata -o /etc/logrotate.d/suricata & pid6=$!
+
+# Attach the SPAN interfaces to the container
+echo attaching the SPAN interfaces
+
+cat /home/uns/span_interfaces.txt | while read line
+do sudo docker network connect $line'_span' IDS
+done
+
+echo adding the iptables rules to the new container
+
+# Enable Suricata iptable rules eth1
+
+sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth1 -j NFQUEUE"
+
+# Enable Suricata iptable rules eth2
+
+sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth2 -j NFQUEUE"
+
+# Enable Suricata iptable rules eth3
+
+sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth3 -j NFQUEUE"
+
+# Enable Suricata iptable rules eth4
+
+sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth4 -j NFQUEUE"
+
+# Enable Suricata iptable rules eth5
+
+sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth5 -j NFQUEUE"
+
+# Enable suricata
+
+sudo docker exec IDS /bin/bash -c "systemctl enable suricata"
+
+# Start suricata
+
+sudo docker exec IDS /bin/bash -c "service suricata start"
