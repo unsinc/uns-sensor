39ae836c9562cb09c95e6db5231eb1f656534567
unsinc <156654990+unsinc@users.noreply.github.com>
2024-06-14 15:40:39 -0500

    Update patch_1.2.sh

--- configuration/patch_1.2.sh
+++ configuration/patch_1.2.sh
@@ -65,31 +65,31 @@ echo adding the iptables rules to the new container
 
 # Enable Suricata iptable rules eth1
 
-sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth1 -j NFQUEUE"
+#sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth1 -j NFQUEUE"
 
 # Enable Suricata iptable rules eth2
 
-sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth2 -j NFQUEUE"
+#sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth2 -j NFQUEUE"
 
 # Enable Suricata iptable rules eth3
 
-sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth3 -j NFQUEUE"
+#sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth3 -j NFQUEUE"
 
 # Enable Suricata iptable rules eth4
 
-sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth4 -j NFQUEUE"
+#sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth4 -j NFQUEUE"
 
 # Enable Suricata iptable rules eth5
 
-sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth5 -j NFQUEUE"
+#sudo docker exec IDS /bin/bash -c "iptables -I FORWARD -i eth5 -j NFQUEUE"
 
 # Adding iptables persistent 
 
-sudo docker exec IDS /bin/bash -c "apt-get install iptables-persistent -y"
+#sudo docker exec IDS /bin/bash -c "apt-get install iptables-persistent -y"
 
 # Saving iptables file
 
-sudo docker exec IDS /bin/bash -c "iptables-save > /etc/iptables.conf"
+#sudo docker exec IDS /bin/bash -c "iptables-save > /etc/iptables.conf"
 
 # Enable suricata
 
